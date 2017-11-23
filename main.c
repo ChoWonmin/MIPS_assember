59aeb0d701d7e054ae2651cb1fc45ce10afb97a7
wonmin <dnjsalswkd@ajou.ac.kr>
2017-11-23 21:10:59 +0900

    symbol table implement

--- main.c
+++ main.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 typedef struct label{
     int index;
-    char* name;
+    char name[10];
     int value;
 }label;
 
-label scan_label(char* inst, int* labelNum);
 int main(int argc, char* argv[])
 {
     int instNum=100;
     char inst_list[instNum][128];
 
     char file_name[64] = "test01.txt";
-    printf("enter the file name :: ");
-    scanf("%s",file_name);
+    //printf("enter the file name :: ");
+    //scanf("%s",file_name);
 
     int pc=0;
 
-    label label_table[20];
     int labelNum = 0;
+    label symbol_table[20];
     float res[32] = {0.0f};
 
     FILE* fin = fopen(file_name,"r");
@@ -32,41 +30,44 @@ int main(int argc, char* argv[])
     }
 
     int i=0;
-    while (fgets(inst_list[i], 128, fin)) {
-        scan_label(inst_list[i++],&labelNum);
+    char line[128];
+    while (fgets(line, 128, fin)) {
+
+        strcpy(inst_list[i],line);
+        char temp[128];
+        strcpy(temp,line);
+
+        if(strchr(temp,':')!=NULL){ //label이 있는 경우
+            symbol_table[labelNum].index=i;
+            strcpy(symbol_table[labelNum].name,strtok(temp,":"));
+            strcpy(temp,line);
+
+            char* temp2 = strstr(temp,".word");
+            if(temp2){
+                temp2 = strchr(temp2,' ');
+                trim(temp2);
+                symbol_table[labelNum].value = atoi(temp2);
+            }else{
+                symbol_table[labelNum].value = -1;
+            }
+            labelNum++;
+        }
+        i++;
     }
     instNum=i;
 
     for(i=0;i<instNum;i++){
-        printf("%s",inst_list[i]);
+        //printf("%s",inst_list[i]);
+    }
+
+    for(i=0;i<labelNum;i++){
+        printf("%d , %s , %d\n",symbol_table[i].index,symbol_table[i].name, symbol_table[i].value);
     }
 
     printf("\n");
     fclose(fin);
 
     return 0;
-}
-label scan_label(char* inst, int* labelNum){
-
-    label res;
-    char temp[128];
-    strcpy(temp,inst);
-
-    if(strchr(temp,':')!=NULL){
-        res.index = labelNum;
-        res.name = strtok(temp,":");
-        strcpy(temp,inst);
-
-        char* temp2 = strstr(temp,".word");
-        if(temp2){
-            temp2 = strchr(temp2,' ');
-            trim(temp2);
-            res.value = atoi(temp2);
-        }
-    }
-
-    labelNum++;
-    return res;
 }
 int trim (char *line)
 {
